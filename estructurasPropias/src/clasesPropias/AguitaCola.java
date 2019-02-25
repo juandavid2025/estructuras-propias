b08930caa654c0adf6403f16f1bbdcdb88cc5641
juandavid2025 <juan.aguirre1@correo.icesi.edu.co>
2019-02-25 09:52:01 -0500

    ..

---
+++ estructurasPropias/src/clasesPropias/AguitaCola.java
@@ -0,0 +1,27 @@
+package clasesPropias;
+
+import java.util.ArrayList;
+
+public class AguitaCola<T> {
+
+	private ArrayList<T> arrayL;
+	
+ public AguitaCola() {
+	 arrayL = new ArrayList<T>();
+ } 	
+	
+ public void offer(T index) {
+	 arrayL.add(0,index);
+ }
+ 
+ public T poll() {
+	 T dato=arrayL.get(arrayL.size()-1);
+	 arrayL.remove(arrayL.size()-1);
+	 return dato;
+ }
+ 
+ public T peek() {
+	 return arrayL.get(arrayL.size()-1);
+ }
+ 
+}
